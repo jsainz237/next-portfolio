b7d38599604950b4fc903b6d92b69151ce6a5dd0
Jesse Sainz <jsainz237@gmail.com>
2021-11-01 17:52:13 -0500

    Fix globe scroll position on small screens

--- components/Canvas/Globe/index.tsx
+++ components/Canvas/Globe/index.tsx
@@ -2,7 +2,8 @@ import { useMemo } from "react";
 import { MeshProps } from "@react-three/fiber";
 import * as THREE from 'three';
 
-import { useInterpolateScroll } from "@utils/hooks";
+import { useInterpolateScroll, useScreenSize } from "@utils/hooks";
+import { theme } from 'styles/theme';
 import * as shaders from './shaders';
 
 type Props = MeshProps
@@ -11,6 +12,10 @@ export const Globe: React.FC<Props> = (props) => {
     const [yPosition] = useInterpolateScroll([-20, -0.9]);
     const [yRotation] = useInterpolateScroll([-1, 0]);
 
+    const [_, bp] = useScreenSize({});
+    const [yPositionSmall] = useInterpolateScroll([-15, 0.4]);
+    const isSmallScreen = useMemo(() => theme.breakpts[bp] <= theme.breakpts['sm'], [bp]);
+
     const earthMap = useMemo(() => {
         return new THREE.TextureLoader().load('assets/earth-dot-map.png');
     }, []);
@@ -18,7 +23,7 @@ export const Globe: React.FC<Props> = (props) => {
     return (
         <>
             <mesh
-                position={[3.2, yPosition, 0]}
+                position={[3.2, isSmallScreen ? yPositionSmall : yPosition, 0]}
                 rotation={[0, yRotation, 0]}
                 scale={0.27}
                 {...props}
